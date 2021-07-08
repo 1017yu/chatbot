abde07c98d364a5b8e6bfd807d7404a272ed0e1b
1017yu <1017yu@daum.net>
2021-07-08 10:51:46 +0900

    Add button.css

---
+++ button/button.js
@@ -0,0 +1,14 @@
+function mo_btn(){
+  const mo_txt = document.querySelector('#mo_txt');
+  mo_txt.textContent = '모두 함께'
+}
+
+function gak_btn(){
+  const gak_btn = document.querySelector('#gak_txt');
+  gak_btn.textContent = '각자 열심히 공부해서'
+}
+
+function ko_btn(){
+  const ko_btn = document.querySelector('#ko_txt');
+  ko_btn.textContent = '코딩으로 성공하자 :)'
+}
